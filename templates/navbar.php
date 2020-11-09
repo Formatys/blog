917b97d56e4db7cb300dcf1dddd8f38535747799
Mateusz Formela <matysfor@gmail.com>
2020-11-09 22:15:06 +0100

    init templates page

---
+++ templates/navbar.php
@@ -0,0 +1,21 @@
+
+
+    <div class="nav-scroller py-1 mb-2">
+        <nav class="nav d-flex justify-content-between">
+            <a class="p-2 text-muted" href="<?php echo WEB_URL;?>index.php?a=home"> Strona glowna</a>
+<!--            <a class="p-2 text-muted" href="--><?php //echo WEB_URL;?><!--index.php?a=blog"">Blog</a>-->
+            <a class="p-2 text-muted" href="#">Technology</a>
+            <a class="p-2 text-muted" href="#">Design</a>
+            <a class="p-2 text-muted" href="#">Culture</a>
+            <a class="p-2 text-muted" href="#">Business</a>
+            <a class="p-2 text-muted" href="#">Politics</a>
+            <a class="p-2 text-muted" href="#">Opinion</a>
+            <a class="p-2 text-muted" href="#">Science</a>
+            <a class="p-2 text-muted" href="#">Health</a>
+            <a class="p-2 text-muted" href="#">Style</a>
+            <a class="p-2 text-muted" href="#">Travel</a>
+            <a class="p-2 text-muted" href="<?php echo WEB_URL;?>index.php?a=contact"> Kontakt</a>
+
+        </nav>
+    </div>
+
